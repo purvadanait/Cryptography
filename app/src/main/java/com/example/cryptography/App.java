e9e813b889cee2b0b7e494cd479ee7ca9e1e8483
Purva <danaitpurva2001@gmail.com>
2022-08-29 22:21:09 +0530

    main layout

--- app/src/main/java/com/example/cryptography/App.java
+++ app/src/main/java/com/example/cryptography/App.java
@@ -1,4 +1,24 @@
 package com.example.cryptography;
 
-public class App {
+import android.app.Application;
+import android.content.res.Configuration;
+import android.util.Log;
+
+import java.util.Locale;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Configuration newConfig = new Configuration();
+        newConfig.locale = Locale.ENGLISH;
+        super.onConfigurationChanged(newConfig);
+        Locale.setDefault(newConfig.locale);
+        getBaseContext().getResources().updateConfiguration(newConfig,getResources().getDisplayMetrics());
+    }
+    public static void Loger(String m){
+        Log.e("mip tag", m);
+    }
+    public static void ToastMaker
+
 }
