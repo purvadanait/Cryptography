fdb9a8d5ace5b50e39fa706f4a26494eaedb4725
Purva <danaitpurva2001@gmail.com>
2022-08-30 10:39:55 +0530

    main ctivity

--- app/src/main/java/com/example/cryptography/MainActivity.java
+++ app/src/main/java/com/example/cryptography/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.cryptography;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.util.Base64;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,17 +41,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public String encrypt(String value, Context c){
         String crypted="";
         try {
             byte[] cleartext = value.getBytes(StandardCharsets.UTF_8);
             SecretKeySpec key = new SecretKeySpec(keytext.getText().toString().getBytes(),"DES");
             Cipher cipher = Cipher.getInstance("DES/ECB/ZeroBytePadding");
-
-
-
-
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            crypted = Base64.encodeToString(cipher.doFinal(cleartext), Base64.DEFAULT);
         }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            App.DialogMaker(c, "Encrypt Error", "Error"+"\n"+e.getMessage());
+            return "Encrypt Error";
+        }
+        catch (NoSuchPaddingException e) {
+           e.printStackTrace();
+           App.DialogMaker(c, "Encrypt Error", "Error"+"\n"+e.getMessage());
+           return "Encrypt Error";
+        }
+        catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+            App.DialogMaker(c, "Encrypt Error", "Error"+"\n"+e.getMessage());
+            return "Encrypt Error";
+        }
+        catch (BadPaddingException e) {
+            e.printStackTrace();
+            App.DialogMaker(c, "Encrypt Error", "Error"+"\n"+e.getMessage());
+            return "Encrypt Error";
+        }
+        catch (InvalidKeyException e) {
+            e.printStackTrace();
+            App.DialogMaker(c, "Encrypt Error", "Error"+"\n"+e.getMessage());
+            return "Encrypt Error";
+        } catch (Exception e) {
+            e.printStackTrace();
+            App.DialogMaker(c, "Encrypt Error", "Error"+"\n"+e.getMessage());
+            return "Encrypt Error";
+        }
+        return crypted;
 
 
 
