a4df0b85971625f575aa7281787f8ce79f8f74a4
Purva <danaitpurva2001@gmail.com>
2022-08-30 10:08:01 +0530

    main ctivity

--- app/src/main/java/com/example/cryptography/MainActivity.java
+++ app/src/main/java/com/example/cryptography/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.cryptography;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity {
+    EditText keytext;
+    EditText normaltext;
+    EditText ciphertext;
+    Button copy_normal;
+    Button copy_cipher;
+    Button encrypt;
+    Button decrypt;
+    Button delete_normal;
+    Button delete_cipher;
+    TextView char_count;
+    TextView char_count2;
+    Context c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public String encrypt(String value, Context c){
+        String crypted="";
+        try {
+            byte[] cleartext = value.getBytes(StandardCharsets.UTF_8);
+            SecretKeySpec key = new SecretKeySpec(keytext.getText().toString().getBytes(),"DES");
+            Cipher cipher = Cipher.getInstance("DES/ECB/ZeroBytePadding");
+
+
+
+
+        }
+
+
+
     }
 }
