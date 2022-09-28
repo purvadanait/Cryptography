5689f8ff3d6b7d942d26c2fcbd194d612debcf34
Purva <danaitpurva2001@gmail.com>
2022-09-28 11:07:07 +0530

    initial commit

---
+++ app/src/main/java/com/example/cryptography/start.java
@@ -0,0 +1,30 @@
+package com.example.cryptography;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class start extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        button = findViewById(R.id.text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
